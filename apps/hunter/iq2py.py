55c4ec766ca106a471f983232655380546b95da3
Ryan Tse <rytse@protonmail.com>
2019-05-03 14:43:25 -0400

    Add script to convert IQ format to numpy format

---
+++ apps/hunter/iq2py.py
@@ -0,0 +1,7 @@
+import numpy as np
+import sys
+adj = np.fromfile(sys.argv[1], dtype=np.int16)
+z = adj[0:len(adj)-1:2] + 1.0j * adj[1:len(adj):2]
+z = z.astype(np.complex64)
+np.save(sys.argv[2], z)
+z.tofile(sys.argv[2])
