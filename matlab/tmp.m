ab732fa24863ebda5404053e5db0083967cdce79
Ryan Tse <rytse@protonmail.com>
2019-06-29 19:08:27 -0400

    Gen ionogram in matlab

--- matlab/tmp.m
+++ matlab/tmp.m
@@ -1,12 +1,22 @@
 WLEN = 2^20;
 NW = 128;
 
-%fid = fopen('../data/out/real_nofilt_nodecim.out', 'rb');
-raw = fread(fid, WLEN * NW * 2);
+fid = fopen('../data/out/real_nofilt_nodecim_adj.out', 'rb');
+raw = fread(fid, WLEN * NW * 2, 'float32');
+fclose(fid);
 raw = complex(raw(1:2:end), raw(2:2:end));
 
-spec = fft(raw);
-spec = reshape(spec, WLEN, length(spec) / WLEN);
-out = real(diff(sum(spec')));
+f = fwf(raw, 50e6, WLEN, 0);
+figure;
 
-plot(out);
+% spec = fft(raw);
+% spec = reshape(spec, WLEN, length(spec) / WLEN);
+% out = real(diff(sum(spec')));
+
+out = real(diff(sum(f')));
+
+out = out / max(out);
+bfs = linspace(0, 50e6, length(f));
+plot(bfs(1:end-1)/1e6, out);
+xlabel('Beat Frequency (MHz)');
+ylabel('Waterfall Row Sum (Normalized)');
