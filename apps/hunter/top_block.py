f15cebe3e981d33a2c786d4ab26ea680abfb923b
Ryan Tse <rytse@protonmail.com>
2019-06-29 19:05:33 -0400

    Correctly mix and filter+decim for ionogram gen

--- apps/hunter/top_block.py
+++ apps/hunter/top_block.py
@@ -20,6 +20,10 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
 from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio.filter import firdes
@@ -27,10 +31,10 @@ import sip
 from gnuradio import blocks
 from gnuradio import filter
 from gnuradio import gr
-import sys
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from iqsource import iqsource  # grc-generated hier_block
 import chirphunter
 import numpy as np
 from gnuradio import qtgui
@@ -72,15 +76,20 @@ class top_block(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 50e6
-        self.decim = decim = 500
-        self.lpf_taps = lpf_taps = firdes.low_pass(1.0, samp_rate / decim, samp_rate / decim / 2,0.1e3, firdes.WIN_HAMMING, 6.76)
-        self.fn = fn = '/media/rytse/Seagate Backup Plus Drive/Flash Drive/cubesat/new sh recordings/will not copy/IQREC-02-03-19-14h02m40s879.iq'
+        self.lpf = lpf = firdes.low_pass(1.0, samp_rate, 50e6 / 100,50e3, firdes.WIN_HAMMING, 6.76)
+        self.fn = fn = '/media/rytse/Seagate Backup Plus Drive/Flash Drive/cubesat/new sh recordings/will not copy/IQREC-02-03-19-12h31m01s566.iq'
         self.f0 = f0 = 6043612
+        self.decim = decim = 500
         self.chirp_rate = chirp_rate = 9.9951e4
 
         ##################################################
         # Blocks
         ##################################################
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=100,
+                taps=lpf,
+                fractional_bw=None)
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_f(
             8192, #size
             firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -115,51 +124,28 @@ class top_block(gr.top_block, Qt.QWidget):
 
         self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
-        self.fir_filter_xxx_1 = filter.fir_filter_ccc(decim, lpf_taps)
-        self.fir_filter_xxx_1.declare_sample_delay(0)
-        self.chirphunter_chirpgen_1 = chirphunter.chirpgen(f0  * 1.005 * 1.5 * 2, chirp_rate, samp_rate)
-        self.chirphunter_chirpgen_0 = chirphunter.chirpgen(f0 , chirp_rate, samp_rate)
+        self.iqsource_1 = iqsource(
+            fn=fn,
+        )
+        self.chirphunter_chirpgen_1 = chirphunter.chirpgen(f0 +29.7e6 + 49.3e6, chirp_rate, samp_rate)
         self.blocks_multiply_xx_1 = blocks.multiply_vcc(1)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(100000000)
-        self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/rytse/Documents/cubesat/gr-chirphunter/data/out/sim_real_raw.out', False)
+        self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/rytse/Documents/cubesat/gr-chirphunter/data/out/real_nofilt_nodecim_adj.out', False)
         self.blocks_file_sink_1.set_unbuffered(False)
         self.blocks_conjugate_cc_0 = blocks.conjugate_cc()
         self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
-        self.band_pass_filter_0 = filter.fir_filter_ccf(
-            1,
-            firdes.band_pass(
-                1,
-                samp_rate,
-                6e6,
-                7e6,
-                0.1e6,
-                firdes.WIN_HAMMING,
-                6.76))
-
-        tmp = firdes.band_pass(
-                1,
-                samp_rate,
-                6e6,
-                7e6,
-                0.1e6,
-                firdes.WIN_HAMMING,
-                6.76)
-        print(f'\n\n\n\nBPF LENGTH: {len(tmp)}\n\n\n\n')
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.band_pass_filter_0, 0), (self.blocks_complex_to_real_0, 0))
-        self.connect((self.band_pass_filter_0, 0), (self.blocks_multiply_xx_1, 0))
         self.connect((self.blocks_complex_to_real_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
         self.connect((self.blocks_conjugate_cc_0, 0), (self.blocks_multiply_xx_1, 1))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.blocks_multiply_xx_1, 0), (self.fir_filter_xxx_1, 0))
-        self.connect((self.chirphunter_chirpgen_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_xx_1, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.chirphunter_chirpgen_1, 0), (self.blocks_conjugate_cc_0, 0))
-        self.connect((self.fir_filter_xxx_1, 0), (self.blocks_file_sink_1, 0))
+        self.connect((self.iqsource_1, 0), (self.blocks_multiply_xx_1, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_complex_to_real_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_file_sink_1, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -171,33 +157,34 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, 6e6, 7e6, 0.1e6, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 8e6, 0.1e6, firdes.WIN_HAMMING, 6.76))
         self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.samp_rate)
 
-    def get_decim(self):
-        return self.decim
+    def get_lpf(self):
+        return self.lpf
 
-    def set_decim(self, decim):
-        self.decim = decim
-
-    def get_lpf_taps(self):
-        return self.lpf_taps
-
-    def set_lpf_taps(self, lpf_taps):
-        self.lpf_taps = lpf_taps
-        self.fir_filter_xxx_1.set_taps(self.lpf_taps)
+    def set_lpf(self, lpf):
+        self.lpf = lpf
+        self.rational_resampler_xxx_0.set_taps(self.lpf)
 
     def get_fn(self):
         return self.fn
 
     def set_fn(self, fn):
         self.fn = fn
+        self.iqsource_1.set_fn(self.fn)
 
     def get_f0(self):
         return self.f0
 
     def set_f0(self, f0):
         self.f0 = f0
+
+    def get_decim(self):
+        return self.decim
+
+    def set_decim(self, decim):
+        self.decim = decim
 
     def get_chirp_rate(self):
         return self.chirp_rate
